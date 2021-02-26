5fc724613f01f69c5fb51dc88c5e3949b896158f
unknown <hchen63@buffalo.edu>
2021-02-25 23:34:39 -0500

    tutorial 1

--- server/index.js
+++ server/index.js
@@ -2,28 +2,39 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 
-const clientPath = path.join(__dirname, '../client')
-const staticPath = path.join(clientPath,'/static');
-const viewsPath = path.join(clientPath,'/views')
+const clientPath = path.join(__dirname, '../client/')
+const staticPath = path.join(clientPath,'/static/');
+const viewsPath = path.join(clientPath,'/views/')
 
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
 
+var x = 0;
+
+const counter = function(req,res,next){
+    x++;
+    console.log(x);
+    next();
+}
+
+app.use(counter);
+app.use(express.static(staticPath));
+
 //this takes functions and response
 app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/monkeflip', function(req, res){
+app.get('/monkeflip', counter, function(req, res){
     res.render('monkeflip');
 });
 
-app.get('/monketypebeat', function(req, res){
+app.get('/monkeMusic', counter, function(req, res){
     res.render('monkeMusic');
 });
 
-app.use(express.static(staticPath));
+
 
 app.listen(2000);
