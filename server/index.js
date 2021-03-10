6e8391c3846bd7ac566b46c9b15b3f51ec62b83d
Hang Chen <hchen63@buffalo.edu>
2021-03-10 00:59:45 -0500

    t2 update

--- server/index.js
+++ server/index.js
@@ -1,40 +1,47 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
 
 const clientPath = path.join(__dirname, '../client/')
 const staticPath = path.join(clientPath,'/static/');
 const viewsPath = path.join(clientPath,'/views/')
 
 const app = express();
+app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    name: 'monke',
+    secert: 'monkegetabanana',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000*60*60*24*3,
+    }
+}));
+app.listen(2000);
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
 
-var x = 0;
-
-const counter = function(req,res,next){
-    x++;
-    console.log(x);
-    next();
-}
-
-app.use(counter);
-app.use(express.static(staticPath));
-
 //this takes functions and response
+
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', {nomen:  req.session.userName});
 });
 
-app.get('/monkeflip', counter, function(req, res){
+app.get('/monkeflip', function(req, res){
     res.render('monkeflip');
 });
 
-app.get('/monkeMusic', counter, function(req, res){
+app.get('/monkeMusic', function(req, res){
     res.render('monkeMusic');
 });
 
+app.post('/welcome', (req, res) => {
+    req.session.userName = req.body.visitorname;
+    res.redirect('/');
+});
 
-
-app.listen(2000);
